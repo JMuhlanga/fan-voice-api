a367a30c28afe65edb33ce8f5c9b681779dcd5e8
Jose Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-12-14 14:14:34 +0300

    Some Modifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,4 +29,5 @@ class UsersController < ApplicationController
     def authorize 
         return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     end
+    
 end
